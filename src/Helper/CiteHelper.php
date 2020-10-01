321498588c32770a35749c1948aeff016b883142
Norbert Czirjak <Norbert.Czirjak@oeaw.ac.at>
2020-10-01 12:35:14 +0200

    cite fix

--- src/Helper/CiteHelper.php
+++ src/Helper/CiteHelper.php
@@ -63,6 +63,30 @@ class CiteHelper
         }
         return $result;
     }
+    
+    /**
+     * 
+     * Remove the duplications
+     * 
+     * @param array $arr1
+     * @param array $arr2
+     * @return string
+     */
+    private function removeDuplication(array $arr1, array $arr2): string {
+        
+        $intersecArray = array();
+        $str = "";
+        $intersecArray = array_intersect($arr1,$arr2);
+        if(count($intersecArray) > 0) {
+            foreach($intersecArray as $v) {
+                foreach (array_keys($arr2, $v, true) as $key) {
+                    unset($arr2[$key]);
+                }
+            }
+            $str = implode(', ', $arr2);
+        }
+        return $str;
+    }
 
     /**
      * Create the Collection / project Cite widget
@@ -79,6 +103,7 @@ class CiteHelper
         $authors = "";
         $creators = "";
         $authors = $this->getCiteWidgetData($this->obj, "acdh:hasAuthor");
+
         if (!empty($authors)) {
             $this->cite["MLA"]["authors"] = $authors;
             $this->cite["MLA"]["string"] = $authors;
@@ -92,15 +117,30 @@ class CiteHelper
                 $this->cite["MLA"]["string"] .= ". ";
             }
         }
-
+        
         //get hasPrincipalInvestigator
         $hasPrincipalInvestigator = $this->getCiteWidgetData($this->obj, "acdh:hasPrincipalInvestigator");
         if (!empty($hasPrincipalInvestigator)) {
+                        
+            $str = "";
             //remove the . and change it to ,
             $this->cite["MLA"]["string"] = substr($this->cite["MLA"]["string"], 0, -2);
-            $this->cite["MLA"]["string"] .= ", ";
-            $this->cite["MLA"]["hasPrincipalInvestigator"] = $hasPrincipalInvestigator;
-            $this->cite["MLA"]["string"] .= $hasPrincipalInvestigator;
+            
+            //if we already have an author/creator, then we need to filter out the duplications
+            if(!empty($this->cite["MLA"]["string"])) {
+                if (strpos((string)$hasPrincipalInvestigator, strval($this->cite["MLA"]["string"])) !== false) {
+                    $str = $this->removeDuplication(explode(',',$this->cite["MLA"]["string"]),  explode(',',$hasPrincipalInvestigator));
+                }
+            }else {
+                $str = $hasPrincipalInvestigator;
+            }
+            //if we already have an author and now we have a prinInv then use the ,
+            if(!empty($this->cite["MLA"]["string"]) && !empty($str)) {
+               $this->cite["MLA"]["string"] .= ", "; 
+            }
+            
+            $this->cite["MLA"]["hasPrincipalInvestigator"] = $str;
+            $this->cite["MLA"]["string"] .= $str;
             $this->cite["MLA"]["string"] .= ". ";
         }
 
@@ -129,7 +169,6 @@ class CiteHelper
             $this->cite["MLA"]["string"] .= ". ";
         }
 
-
         $this->cite["MLA"]["string"] .= "Accessed on " . date('d M Y') . ".";
         return $this->cite;
     }
@@ -148,7 +187,6 @@ class CiteHelper
     public function createCiteWidgetResourceMetadata(object $topCollection): array
     {
         //hasAuthor/hasCreator. hasTitle. Digitised by hasDigitisingAgent. In: hasCreator(TopCollection), hasPrincipleInvestigator(TopCollection), hasTitle(TopCollection), hasHosting, hasPid. Accessed on "current date".
-
         $this->cite["MLA"]["string"] = "";
         
         //get authors/creators
@@ -227,184 +265,5 @@ class CiteHelper
 
         $this->cite["MLA"]["string"] .= "Accessed on " . date('d M Y') . ".";
         return $this->cite;
-    }
-
-    /**
-     *
-     * Create the HTML content of the cite-this widget on single resource view
-     *
-     * @return array $this->cite Returns the cite-this widget as HTML
-     */
-    public function createCiteThisWidget(): array
-    {
-        $content = [];
-
-        /** MLA Format
-         * Example:
-         * Mörth, Karlheinz. Dictionary Gate. ACDH, 2013, hdl.handle.net/11022/0000-0000-001B-2. Accessed 12 Oct. 2017.
-         */
-        $this->cite["MLA"] = [
-            "authors" => "", "creators" => "", "hasPrincipalInvestigator" => "",
-            "contributors" => "", "title" => "", "isPartOf" => "", "availableDate" => "",
-            "hasHosting" => "", "hasEditor" => "", "accesedDate" => "", "acdhURI" => ""
-        ];
-
-        //Get authors(s)
-        $authors = "";
-        $authors = $this->getCiteWidgetData($this->obj, "acdh:hasAuthor");
-        if (!empty($authors)) {
-            $this->cite["MLA"]["authors"] = $authors;
-        }
-
-        //Get creator(s)
-        $creators = "";
-        $creators = $this->getCiteWidgetData($this->obj, "acdh:hasCreator");
-        if (!empty($creators)) {
-            $this->cite["MLA"]["creators"] = $creators;
-        }
-
-        //Get contributor(s)
-        $contributors = "";
-        $contributors = $this->getCiteWidgetData($this->obj, "acdh:hasContributor");
-        if (!empty($contributors)) {
-            $this->cite["MLA"]["contributors"] = $contributors;
-        }
-
-        //Get PrincipalInvestigator(s)
-        $principalInvestigator = "";
-        $principalInvestigator = $this->getCiteWidgetData($this->obj, "acdh:hasPrincipalInvestigator");
-        if (!empty($principalInvestigator)) {
-            $this->cite["MLA"]["hasPrincipalInvestigator"] = $principalInvestigator;
-        }
-
-        //Get title
-        if (!empty($this->obj->getTitle())) {
-            $this->cite["MLA"]["title"] = $this->obj->getTitle();
-        }
-
-        //Get isPartOf
-        if (!empty($this->obj->getData("acdh:isPartOf"))) {
-            $isPartOf = $this->obj->getData("acdh:isPartOf")[0]->title;
-            $this->cite["MLA"]["isPartOf"] = $isPartOf;
-        }
-
-        //Get hasHosting
-        if (!empty($this->obj->getData("acdh:hasHosting"))) {
-            $hasHosting = $this->obj->getData("acdh:hasHosting")[0]->title;
-            $this->cite["MLA"]["hasHosting"] = $hasHosting;
-        }
-
-        /* Get hasPid & create copy link
-         * Order of desired URIs:
-         * PID > id.acdh > id.acdh/uuid > long gui url
-         */
-        if (!empty($this->obj->getPID())) {
-            $this->cite["MLA"]["acdhURI"] = $this->obj->getUUID();
-        }
-
-        if (!$this->cite["MLA"]["acdhURI"]) {
-            if (!empty($this->obj->getIdentifiers()) && count($this->obj->getIdentifiers()) > 0) {
-                $acdhURIs = $this->obj->getIdentifiers();
-                //Only one value under acdh:hasIdentifier
-                $uuid = "";
-
-                foreach ($acdhURIs as $id) {
-                    if (isset($id->value)) {
-                        if (strpos($id->value, $this->repo->getSchema()->__get('drupal')->uuidNamespace) !== false) {
-                            $uuid = $id->value;
-                        //if the identifier is the normal acdh identifier then return it
-                        } elseif (strpos($id->value, $this->repo->getSchema()->__get('id')) !== false) {
-                            $uuid = $id->value;
-                            break;
-                        }
-                    }
-                }
-                $this->cite["MLA"]["acdhURI"] = $uuid;
-            }
-        }
-
-        //Get available date
-        if (!empty($this->obj->getData("acdh:hasAvailableDate"))) {
-            $availableDate = $this->obj->getData("acdh:hasAvailableDate")[0]->title;
-            $availableDate = strtotime($availableDate);
-            $this->cite["MLA"]["availableDate"] = date('Y', $availableDate);
-        }
-
-        //Get accesed date
-        $this->cite["MLA"]["accesedDate"] = date('d M Y');
-
-
-        //Process MLA
-        //Top level resource
-        //if (!$this->cite["MLA"]["isPartOf"]) {
-
-        $this->cite["MLA"]["string"] = "";
-        //AUTHORS
-        if (isset($this->cite["MLA"]["authors"]) && !empty($this->cite["MLA"]["authors"])) {
-            $this->cite["MLA"]["string"] .= $this->cite["MLA"]["authors"] . '... ';
-        } elseif (isset($this->cite["MLA"]["creators"]) && !empty($this->cite["MLA"]["creators"])) {
-            $this->cite["MLA"]["string"] .= $this->cite["MLA"]["creators"] . '. ';
-        } elseif (isset($this->cite["MLA"]["contributors"]) && !empty($this->cite["MLA"]["contributors"])) {
-            $this->cite["MLA"]["string"] .= $this->cite["MLA"]["contributors"] . '. ';
-        }
-
-        //hasPrincipalInvestigator
-        if (
-                isset($this->cite["MLA"]["hasPrincipalInvestigator"]) &&
-                !empty(trim($this->cite["MLA"]["hasPrincipalInvestigator"]))) {
-            $this->cite["MLA"]["string"] = str_replace(".", ",", $this->cite["MLA"]["string"]);
-
-            $arr = explode(",", $this->cite["MLA"]["string"]);
-            foreach ($arr as $a) {
-                $a = ltrim($a);
-                //if the string already contains the prininv name, then we will skip it from the final result
-                if (!empty($a) && strpos($this->cite["MLA"]["hasPrincipalInvestigator"], $a) !== false) {
-                    $this->cite["MLA"]["hasPrincipalInvestigator"] = str_replace($a . ",", "", $this->cite["MLA"]["hasPrincipalInvestigator"]);
-                    $this->cite["MLA"]["hasPrincipalInvestigator"] = str_replace($a, "", $this->cite["MLA"]["hasPrincipalInvestigator"]);
-                }
-            }
-
-            //$this->cite["MLA"]["hasPrincipalInvestigator"] = substr(rtrim($this->cite["MLA"]["hasPrincipalInvestigator"]), 0, -1);
-            if (isset($this->cite["MLA"]["hasPrincipalInvestigator"]) && !empty(trim($this->cite["MLA"]["hasPrincipalInvestigator"]))) {
-                //if the last char is the , then we need to remove it
-                if (substr(trim($this->cite["MLA"]["hasPrincipalInvestigator"]), -1) == ",") {
-                    $this->cite["MLA"]["hasPrincipalInvestigator"] = trim($this->cite["MLA"]["hasPrincipalInvestigator"]);
-                    $this->cite["MLA"]["hasPrincipalInvestigator"] = rtrim($this->cite["MLA"]["hasPrincipalInvestigator"], ",");
-                }
-                $this->cite["MLA"]["string"] .= ' ' . $this->cite["MLA"]["hasPrincipalInvestigator"] . '. ';
-            }
-        }
-
-        if (substr(trim($this->cite["MLA"]["string"]), -1) == ",") {
-            $this->cite["MLA"]["string"] = trim($this->cite["MLA"]["string"]);
-            $this->cite["MLA"]["string"] = rtrim($this->cite["MLA"]["string"], ",");
-            $this->cite["MLA"]["string"] .= '. ';
-        }
-
-        //TITLE
-        if ($this->cite["MLA"]["title"]) {
-            $this->cite["MLA"]["string"] .= '<em>' . $this->cite["MLA"]["title"] . '.</em> ';
-        }
-
-        //PUBLISHER
-        if ($this->cite["MLA"]["hasHosting"]) {
-            $this->cite["MLA"]["string"] .= $this->cite["MLA"]["hasHosting"] . ', ';
-        }
-
-        //DATE
-        if ($this->cite["MLA"]["availableDate"]) {
-            $this->cite["MLA"]["string"] .= $this->cite["MLA"]["availableDate"] . ', ';
-        }
-
-        //HANDLE
-        if ($this->cite["MLA"]["acdhURI"]) {
-            $this->cite["MLA"]["string"] .= $this->cite["MLA"]["acdhURI"] . '. ';
-        }
-
-        //DATE
-        if ($this->cite["MLA"]["accesedDate"]) {
-            $this->cite["MLA"]["string"] .= 'Accessed ' . date('d M Y') . '. ';
-        }
-        return $this->cite;
-    }
+    }    
 }
