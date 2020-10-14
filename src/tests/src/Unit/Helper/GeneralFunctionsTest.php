c54257605593dbe3c9081cfbabd53952d7d3c4b5
Norbert Czirjak <Norbert.Czirjak@oeaw.ac.at>
2020-10-14 14:13:48 +0200

    tests improved

--- src/tests/src/Unit/Helper/GeneralFunctionsTest.php
+++ src/tests/src/Unit/Helper/GeneralFunctionsTest.php
@@ -4,6 +4,7 @@ namespace Drupal\Tests\acdh_repo_gui\Unit;
 
 use acdhOeaw\acdhRepoLib\Repo;
 
+
 /**
  * Tests GeneralFunctions
  *
@@ -30,25 +31,24 @@ class GeneralFunctionsTest extends \PHPUnit\Framework\TestCase
         $this->object = new \Drupal\acdh_repo_gui\Helper\GeneralFunctions(dirname(__DIR__, 1) . '/testconfig.yaml');
     }
 
-    public function testDetailViewUrlDecodeEncode()
-    {
+    public function testDetailViewUrlDecodeEncode() {
         $this->assertEmpty($this->object->detailViewUrlDecodeEncode('', 0));
-        $this->assertEquals('https://id.acdh.oeaw.ac.at/test/H115', $this->object->detailViewUrlDecodeEncode('id.acdh.oeaw.ac.at:test:H115', 0));
-        $this->assertEquals('https://id.acdh.oeaw.ac.at/uuid/test/H115', $this->object->detailViewUrlDecodeEncode('id.acdh.oeaw.ac.at:uuid:test:H115', 0));
-        $this->assertEquals('http://127.0.0.1/api/263325', $this->object->detailViewUrlDecodeEncode('263325', 0));
+        $this->assertEquals('https://id.acdh.oeaw.ac.at/test/H115', $this->object->detailViewUrlDecodeEncode('id.acdh.oeaw.ac.at:test:H115', 0)); 
+        $this->assertEquals('https://id.acdh.oeaw.ac.at/uuid/test/H115', $this->object->detailViewUrlDecodeEncode('id.acdh.oeaw.ac.at:uuid:test:H115', 0)); 
+        $this->assertEquals('http://127.0.0.1/api/263325', $this->object->detailViewUrlDecodeEncode('263325', 0)); 
  
         $stub = $this->createStub(\Drupal\acdh_repo_gui\Helper\GeneralFunctions::class);
         //$stub->method('detailViewUrlDecodeEncode')->willReturn('http://hdl.handle.net/263325');
-        //$this->assertEquals('http://hdl.handle.net/263325', $stub->detailViewUrlDecodeEncode('hdl.handle.net:263325', 0));
+        //$this->assertEquals('http://hdl.handle.net/263325', $stub->detailViewUrlDecodeEncode('hdl.handle.net:263325', 0)); 
         
         // 263325 ->https://arche-dev.acdh-dev.oeaw.ac.at/api/263325  0
         $map = [
             ['hdl.handle.net:263325', 0, 'http://hdl.handle.net/263325'],
             ['geonames.org:263325', 0, 'http://geonames.org/263325'],
             ['d-nb.info:263325', 0, 'http://d-nb.info/263325']
-        ];
+        ];  
         
-        $stub->method('detailViewUrlDecodeEncode')
+         $stub->method('detailViewUrlDecodeEncode')
              ->will($this->returnValueMap($map));
 
         // $stub->doSomething() returns different values depending on
@@ -66,5 +66,32 @@ class GeneralFunctionsTest extends \PHPUnit\Framework\TestCase
         ->will($this->returnValue('http://d-nb.info/263325'));
         
         $this->assertSame('http://d-nb.info/263325', $service->detailViewUrlDecodeEncode('d-nb.info:263325', 0));
+        
+        $this->assertSame('hdl.handle.net/263325', $this->object->detailViewUrlDecodeEncode('http://hdl.handle.net/263325', 1));
+        $this->assertSame('263325', $this->object->detailViewUrlDecodeEncode('http://127.0.0.1/api/263325', 1));
+        $this->assertSame('example.com/263325', $this->object->detailViewUrlDecodeEncode('https://example.com/263325', 1));
+        $this->assertSame('example.com/263325', $this->object->detailViewUrlDecodeEncode('http://example.com/263325', 1));
+        
     }
+    
+    public static function exampleUUIDData() {
+        return (object) array('id' => '263325');
+    }
+   
+    public function testSpecialIdentifierToUUID()
+    {
+       
+        $service = $this->getMockBuilder(\Drupal\acdh_repo_gui\Model\GeneralFunctionsModel::class)
+        ->disableOriginalConstructor()
+        ->setMethods(['getViewData'])
+        ->getMock();
+        
+        $service->expects($this->any())
+        ->method('getViewData')
+        ->willReturn(array(self::exampleUUIDData()));
+     
+        $this->assertEquals(array(self::exampleUUIDData()), $service->getViewData());
+        
+    }
+    
 }
