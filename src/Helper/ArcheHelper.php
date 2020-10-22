0852891318552f6b6786c2b67040f5f5debf9598
Norbert Czirjak <Norbert.Czirjak@oeaw.ac.at>
2020-10-22 14:37:14 +0200

    refactor

--- src/Helper/ArcheHelper.php
+++ src/Helper/ArcheHelper.php
@@ -90,6 +90,61 @@ abstract class ArcheHelper
     }
     
     /**
+     * Set the language tag for the object
+     * @param object $obj
+     * @return string
+     */
+    private function setLanguage(object $obj): string
+    {
+        $lang = 'en';
+        if (isset($obj->language) && (!empty($obj->language))) {
+            $lang = $obj->language;
+        } else {
+            $lang = $this->siteLang;
+        }
+        
+        return $lang;
+    }
+    
+    private function setUpRepoIdForExternalType(object &$d): void
+    {
+        if (isset($d->type) && !empty($d->type) && $d->type == "REL") {
+            $d->repoid = "";
+            $d->repoid = $d->value;
+            $d->value = $d->relvalue;
+        }
+    }
+    
+    private function setTitle(object &$d): void
+    {
+        if( (isset($d->title) && empty($d->title)) ||
+            !isset($d->title) ) {
+            $d->title = "";
+            $d->title = $d->value;
+        }
+        
+        if (isset($d->relvalue) && !empty($d->relvalue)) {
+            $d->title = $d->relvalue;
+        }
+    }
+    
+    private function setAcdhId(object &$d): void
+    {
+       if (isset($d->acdhid) && !empty($d->acdhid)) {
+            $d->insideUri = "";
+            $d->insideUri = $this->makeInsideUri($d->acdhid);
+        } 
+    }
+    
+    private function setUri(object &$d): void
+    {
+        if (isset($d->vocabsid) && !empty($d->vocabsid)) {
+            $d->uri = $d->vocabsid;
+            unset($d->insideUri);
+        }
+
+    }
+    /**
      * Extend the actual object with the shortcuts
      *
      */
@@ -100,44 +155,33 @@ abstract class ArcheHelper
         foreach ($this->data as $d) {
             
             //add the language to every resource
-            $lang = 'en';
-            if (isset($d->language) && (!empty($d->language))) {
-                $lang = $d->language;
-            } else {
-                $lang = $this->siteLang;
-            }
+            $lang = $this->setLanguage($d);
+            
             // if we have an external type then the repoid will be the value
             //because we will use their own id for the  linking
             //and the value will be the relvalue because of the relational data
-            if (isset($d->type) && !empty($d->type) && $d->type == "REL") {
-                $d->repoid = "";
-                $d->repoid = $d->value;
-                $d->value = $d->relvalue;
-            }
-            
+            $this->setUpRepoIdForExternalType($d);
+           
             if (is_null($d->property) === false) {
-                //create the shortcur
-                $d->title = "";
-                $d->title = $d->value;
-                if (isset($d->relvalue) && !empty($d->relvalue)) {
-                    $d->title = $d->relvalue;
-                }
-                if (isset($d->acdhid) && !empty($d->acdhid)) {
-                    $d->insideUri = "";
-                    $d->insideUri = $this->makeInsideUri($d->acdhid);
-                }
+                //create the shortcut
+                $this->setTitle($d);
+                
+                $this->setAcdhId($d);
+                
                 $d->shortcut = $this->createShortcut($d->property);
                 
                 //if we have vocabsid then it will be the uri, to forward the users to the vocabs website
-                if (isset($d->vocabsid) && !empty($d->vocabsid)) {
-                    $d->uri = $d->vocabsid;
-                    unset($d->insideUri);
-                }
+                $this->setUri($d);
                 
                 //check and remove the duplicated values from the results
                 if (!isset($result[$d->shortcut][$lang])) {
                     $result[$d->shortcut][$lang][] = $d;
-                } elseif (isset($d->repoid) && isset($result[$d->shortcut][$lang]) && (count($result[$d->shortcut][$lang]) > 0)) {
+                } elseif (isset($result[$d->shortcut][$lang]) && (count($result[$d->shortcut][$lang]) > 0)) {
+                    //if we already have a shortcut in the results then we need to check if it id a relation 
+                    //or any other type
+                    
+                    //this is a relation so we need to check the duplicates
+                    if(isset($d->repoid)){
                     //we ahve shorcut and repoid and already results in the result array
                     $searchedValue = $d->repoid;
                     $res = array();
@@ -151,24 +195,26 @@ abstract class ArcheHelper
                             }
                         }
                     );
+
                     //if we have new value for the same shortcut then add it to the array
                     if (isset($res[0]->repoid)) {
                         $result[$d->shortcut][$lang][] = $d;
                     }
+                    }else {
+                        //simple string or date, then we simply extending the actual results array
+                        $result[$d->shortcut][$lang][] = $d;
                 }
+                }
+                
             } elseif (isset($d->type) && !empty($d->type) && $d->type == "ID") {
                 //setup the acdh uuid variable
                 $d->property = 'https://vocabs.acdh.oeaw.ac.at/schema#hasIdentifier';
-                if (strpos($d->value, '/id.acdh.oeaw.ac.at/uuid/') !== false) {
-                    $d->acdhid = $d->value;
-                }
-                if (strpos($d->value, '//repo.') !== false) {
-                    $d->repoid = $d->id;
-                }
+                
+                $this->setIDTypeAcdhId($d);
+                $this->setIDTypeRepoId($d);
+                
                 //the uri for the identifier urls
-                if (strpos($d->value, 'http') !== false) {
-                    $d->uri = $d->value;
-                }
+                $this->setIDTypeUri($d);
                 //add the identifier into the final data
                 $result['acdh:hasIdentifier'][$lang][] = $d;
             }
@@ -179,6 +225,27 @@ abstract class ArcheHelper
         }
         
         $this->data = $result;
+    }
+    
+    private function setIDTypeAcdhId(object &$d): void
+    {
+        if (strpos($d->value, '/id.acdh.oeaw.ac.at/uuid/') !== false) {
+            $d->acdhid = $d->value;
+        } 
+    }
+    
+    private function setIDTypeRepoId(object &$d): void
+    {
+        if (strpos($d->value, '//repo.') !== false) {
+            $d->repoid = $d->id;
+        }
+    }
+    
+    private function setIDTypeUri(object &$d): void
+    {
+        if (strpos($d->value, 'http') !== false) {
+            $d->uri = $d->value;
+        }
     }
     
     abstract public function createView(): array;
