75763cb04116e39688f2118b7bb77370d1ea5372
Norbert Czirjak <Norbert.Czirjak@oeaw.ac.at>
2020-11-26 10:15:18 +0100

    phpunit test

--- src/tests/src/Unit/Helper/ArcheHelperTest.php
+++ src/tests/src/Unit/Helper/ArcheHelperTest.php
@@ -1,19 +1,49 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Drupal\Tests\acdh_repo_gui\Unit\Helper;
 
-namespace Drupal\Tests\acdh_repo_gui\Unit;
+require_once dirname(__DIR__, 1) . '/PHPUnitUtil.php';
 
 /**
- * Description of ArcheHelperTest
+ * Tests ArcheHelper
  *
- * @author nczirjak
+ * @group acdh_repo_gui
+ * @coversDefaultClass \Drupal\acdh_repo_gui\Helper\ArcheHelper
  */
-class ArcheHelperTest
-{
-    //put your code here
+class ArcheHelperTest extends \PHPUnit\Framework\TestCase {
+    
+    private static $config;
+    private static $repo;
+    private $absObj;
+    
+    protected function setUp(): void {
+        parent::setUp();
+        $this->initAbstract();
+    }
+    
+    public static function setUpBeforeClass(): void
+    {
+        require_once dirname(__DIR__, 5) . '/vendor/autoload.php';
+        $cfgFile = dirname(__DIR__, 1) . '/testconfig.yaml';
+        self::$config = json_decode(json_encode(yaml_parse_file(dirname(__DIR__, 1) . '/testconfig.yaml')));
+        self::$repo = \acdhOeaw\acdhRepoLib\Repo::factory(dirname(__DIR__, 1) . '/testconfig.yaml');
+        
+        
+    }
+    
+    public function initAbstract(): void
+    {
+        $this->absObj = $this->getMockForAbstractClass(\Drupal\acdh_repo_gui\Helper\ArcheHelper::class, [dirname(__DIR__, 1) . '/testconfig.yaml']);
+    }
+   
+    public function testCreateShortCut() {
+       
+        $returnVal = \PHPUnitUtil::callMethod(
+                $this->absObj,
+                'createShortcut', 
+                array('http://www.loc.gov/premis/rdf/v1#premisTest'));
+        $this->assertSame('premis:premisTest', $returnVal);
+    }
+    
 }
+
---
+++ src/tests/src/Unit/PHPUnitUtil.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Description of PHPUnitUtil
+ *
+ * @author nczirjak
+ */
+
+class PHPUnitUtil
+{
+  public static function callMethod($obj, $name, array $args) {
+        $class = new \ReflectionClass($obj);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+        return $method->invokeArgs($obj, $args);
+    }
+}
