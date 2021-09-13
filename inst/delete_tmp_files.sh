704867aebf0dc4801b1397ed8278b1f65789f3e0
Norbert Czirjak <Norbert.Czirjak@oeaw.ac.at>
2021-09-13 09:21:36 +0200

    delete temp files typo fix

--- inst/delete_tmp_files.sh
+++ inst/delete_tmp_files.sh
@@ -12,7 +12,7 @@ fi
 
 ##delete the 3d files
 if [ -d "/home/www-data/gui/web/sites/default/files/tmp_files/" ]; then
-    if [ "$(ls -A /home/www-data/gui/web/sites/default/files/collections/)" ]; then
+    if [ "$(ls -A /home/www-data/gui/web/sites/default/files/tmp_files/)" ]; then
         echo "tmp files deleted"
         find /home/www-data/gui/web/sites/default/files/tmp_files/* -mtime +3 -delete
     fi
